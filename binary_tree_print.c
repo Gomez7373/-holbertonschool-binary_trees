36cac990f62f58fffe4efa88ff2cb16abd35dea1
Gomez7373 <sgc.holberton@gmail.com>
2024-01-04 01:29:39 -0400

    0-binary_tree_node

--- binary_tree_print.c
+++ binary_tree_print.c
@@ -1,15 +1,44 @@
-#include <stdio.h>
 #include "binary_trees.h"
+#include <stdio.h>
 
+/**
+ * binary_tree_print - Prints a binary tree with a specific format
+ * @tree: A pointer to the root node of the tree to print
+ */
 void binary_tree_print(const binary_tree_t *tree)
 {
     if (tree == NULL)
         return;
 
-    binary_tree_print(tree->right);
+    binary_tree_print_recursive(tree, 0, 0);
+}
 
-    printf("%d\n", tree->n);
+/**
+ * binary_tree_print_recursive - Recursively prints a binary tree
+ * @tree: A pointer to the current node
+ * @depth: The depth of the current node in the tree
+ * @is_left: 1 if the current node is the left child, 0 otherwise
+ */
+void binary_tree_print_recursive(const binary_tree_t *tree, size_t depth, int is_left)
+{
+    if (tree == NULL)
+        return;
 
-    binary_tree_print(tree->left);
+    binary_tree_print_recursive(tree->right, depth + 1, 0);
+
+    if (depth == 0)
+        printf(".-------");
+    else
+    {
+        printf("%*s", (int)(depth * 8 - 1), "");
+        if (is_left)
+            printf(".--");
+        else
+            printf("`--");
+    }
+
+    printf("(%03d)\n", tree->n);
+
+    binary_tree_print_recursive(tree->left, depth + 1, 1);
 }
 
